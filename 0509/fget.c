d06ba6b8ad961a437fc0704ba52c439a08756e5a
ur <dorosi150@naver.com>
2022-05-09 20:54:21 +0900

    0509

---
+++ 0509/fget.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LENGTH 256
+int main(int argc, char *argv[]){
+	FILE *fp;
+	char buffer[MAX_LENGTH];
+	int lineCnt = 0;
+	if((fp=fopen("a.txt", "rb")) != NULL){
+		while(fgets(buffer, MAX_LENGTH, fp)){
+			lineCnt++;
+				
+			if(lineCnt == 2){
+				printf("%s", strtok(buffer, "option: "));
+				break;
+			}
+		}
+	}
+	fclose(fp);
+	return 0;
+}
+
